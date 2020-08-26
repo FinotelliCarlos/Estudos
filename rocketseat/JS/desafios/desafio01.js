912d5d1f7b45d1715b0cd81409fdfe1d408359d1
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-08-26 20:17:35 -0300

    Desafio 01 Concluido

--- rocketseat/JS/desafios/desafio01.js
+++ rocketseat/JS/desafios/desafio01.js
@@ -1,1 +1,11 @@
-//
+//Crie uma função que irá exibir (O usuário mora em São Paulo / SP, no bairro Centro, na rua "Rua dos Pinheiros" com nº 1293.)
+
+var endereco = {
+    rua: "Rua dos pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+   };
+
+console.log("O usuário mora em " + (endereco.cidade) + "/" + (endereco.uf) + ", no bairro " + (endereco.bairro) + ", na rua " + (endereco.rua) + " com nº " + (endereco.numero) + ".");
