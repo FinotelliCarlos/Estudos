781ad8a96d0721a8cefa2248183186c7e74da0ad
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-08-26 22:02:59 -0300

    Desafio 05 Concluido

--- rocketseat/JS/desafios/desafio05.js
+++ rocketseat/JS/desafios/desafio05.js
@@ -1,1 +1,21 @@
-//
+//Escrever uma função que escreva 
+//O Diego possui as habilidades: Javascript, ReactJS, Redux
+//O Gabriel possui as habilidades: VueJS, Ruby on Rails, Elixir
+
+var usuarios = [
+    {
+        nome: "Diego",
+        habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+        nome: "Gabriel",
+        habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    }
+];
+
+function usData(dataUs){
+    for (var us of dataUs) {
+        console.log('O', us.nome, 'possui as habilidades: ', us.habilidades.join(', '));
+    }
+}
+usData(usuarios);
