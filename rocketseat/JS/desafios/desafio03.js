457d3467948504e2a3b199736adb9de936d3cddb
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-08-26 21:22:17 -0300

    Desafio 03 Concluido

--- rocketseat/JS/desafios/desafio03.js
+++ rocketseat/JS/desafios/desafio03.js
@@ -1,1 +1,15 @@
-//
+//Escreva uma função que verifique se o vetor de habilidades passado possui a habilidade "Javascript" e retorna um booleano true/false caso exista ou não.
+
+function temHabilidade(skills) {
+    if (skills == "Javascript"){
+        return "true";
+    } else{
+        return "false";
+        }
+}
+var skills = ["Javascript", "ReactJS", "React Native"];
+temHabilidade(skills);
+
+const resultado = temHabilidade("Javascript")
+
+console.log(resultado)
