1b3ab13192c2ff54a769fdb7f82988806398c514
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-09-08 10:38:04 -0300

    Correção function alert

--- Cursos_Rocketseat/JS/manipulandoDom/trabalhandoComDom.js
+++
@@ -1,8 +0,0 @@
-const inputElement = document.querySelector('input[name=nome]');
-const btnElement = document.querySelector('button.botao');
-
-btnElement.onclick = function() {
-    const text = inputElement.value;
-
-    alert(text)
-}
