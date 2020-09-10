7f80ccb4da0ac84d7a47d832d982b0a815ed9cd5
Finotelli, Carlos Eduardo Pereira <63814196+FinotelliCarlos@users.noreply.github.com>
2020-09-09 21:00:58 -0300

    create jobElements.js
    Trabalhando com elementos,
    Criando elemento com a Tag 'a' dentro da 'div#app' atribuindo link e texto do link

---
+++ Cursos_Rocketseat/JS/manipulandoDom/js/jobElements.js
@@ -0,0 +1,8 @@
+var linkElement.document.createElement('a');
+linkElement.setAttibute('href', 'https://www.google.com/');
+
+var textElement.document.creatTextNode('Clique para acessar o Google!');
+textElement.appendChild(linkElement);
+
+var containerElement.document.querySelector('#app');
+containetElement.appendChild(linkElement);
