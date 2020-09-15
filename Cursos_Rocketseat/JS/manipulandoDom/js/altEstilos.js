17159f0b6b898bc2fe753085d903148635480ab5
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-09-14 22:12:39 -0300

    Alterando Estilos através do JS

---
+++ Cursos_Rocketseat/JS/manipulandoDom/js/altEstilos.js
@@ -0,0 +1,6 @@
+var boxElement = document.querySelector('.box');
+
+boxElement.style.width = '120px';
+boxElement.style.height = '50px';
+boxElement.style.backgroundColor = '#777';
+boxElement.style.borderRadius = '10px';
