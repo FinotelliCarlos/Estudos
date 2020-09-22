469d5b1b9a7bb69128a048311967165e161a2496
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-09-22 09:42:49 -0300

    Fim, AppTodos
    Criando 'todos' , definindo texto do item atravéz do campo input e importando para a lista, excluindo 'todo' clicando em excluir , salvando todos os itens do array em localStorage

--- Cursos_Rocketseat/JS/3-appDeTodos/js/todos.js
+++ Cursos_Rocketseat/JS/3-appDeTodos/js/todos.js
@@ -2,32 +2,55 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer Café',
-    'Estudar JavaScript',
-    'Acessar comunidade da Rockeatseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];//renderizando todo do localStorage
 
 function renderTodos() {
     listElement.innerHTML = '';
 
     for (todo of todos) {
-        var todoElement = document.createElement('li');
-        var todoText = document.createTextNode(todo);
+        var todoElement = document.createElement('li');//renderizando todos.
 
-        todoElement.appendChild (todoText);
-        listElement.appendChild (todoElement);
+        var todoText = document.createTextNode(todo);//renderizando todos.
+
+        var linkElement = document.createElement('a');//excluindo todos.
+
+        linkElement.setAttribute('href', '#');//excluindo todos.
+
+        var pos = todos.indexOf(todo);//excluindo todos.
+        linkElement.setAttribute('onclick','deleteTodo(' + pos + ')');//excluindo todos.
+
+        var linkText = document.createTextNode('Excluir');//excluindo todos.
+
+        linkElement.appendChild(linkText);//excluindo todos.
+
+        todoElement.appendChild (todoText);//renderizando todos.
+
+        todoElement.appendChild(linkElement);//excluindo todos.
+
+        listElement.appendChild (todoElement);//renderizando todos.
+
     }
 }
 
-renderTodos();
+renderTodos();//renderizando todos.
 
-function addTodo(){
+function addTodo(){//criando todos
     var todoText = inputElement.value;
 
     todos.push(todoText);
     inputElement.value= '';
     renderTodos();
+    saveToStorage();//salvando todo no localStorage
 };
 
-buttonElement.onclick = addTodo;
+buttonElement.onclick = addTodo;//criando todos
+
+function deleteTodo(pos){//excluindo todos.
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();//salvando todo no localStorage
+}
+
+function saveToStorage(){//salvando todo no localStorage
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
