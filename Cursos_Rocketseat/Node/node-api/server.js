21912a1af08aac537abd6e69859d059eeff0387b
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-02 10:26:40 -0300

    definindo porta da aplicação

---
+++ Cursos_Rocketseat/Node/node-api/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+//importando o express utilizando require
+
+const app = express();
+//constante app executando a função express
+
+app.listen(7050);
+//fazendo com que a aplicação ouça a porta 7050
