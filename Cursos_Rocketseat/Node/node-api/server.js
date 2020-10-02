2f160259d88bc6ec5fce8ecd0dff07bccbc1e54a
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-02 11:41:32 -0300

    introduzindo primeira RES ao Server.js

--- Cursos_Rocketseat/Node/node-api/server.js
+++ Cursos_Rocketseat/Node/node-api/server.js
@@ -4,5 +4,14 @@ const express = require('express');
 const app = express();
 //constante app executando a função express
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+//toda vez que o usuario acessar a rota.
+    //req simboliza a requisição feita ao servidor.
+    //req vai conter todas as informações possiveis desta requisição.
+        //Parametros, corpo da requisição, cabeçalho da requisição, usuario que fez a requisição, autenticação, IP.
+    //res se refere a resposta dada mediante a requisição.
+
 app.listen(7050);
 //fazendo com que a aplicação ouça a porta 7050
