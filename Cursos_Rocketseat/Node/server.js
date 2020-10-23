8a6683da6766d6d51dbe757f166d784d4bec6009
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-23 18:13:05 -0300

    install require-dir + create Product

--- Cursos_Rocketseat/Node/server.js
+++ Cursos_Rocketseat/Node/server.js
@@ -2,17 +2,30 @@ const express = require('express');
 //importando o express
 const mongoose = require('mongoose');
 //importando o mongoose
+const requireDir = require('require-dir');
 
 //Iniciando o App
 const app = express();
 //constante app executando a função express
 
 //Conectando o DB
-mongoose.connect('mongodb://localhost:27017/nodeAPI', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/mongoDB',
+ {useNewUrlParser: true,  useUnifiedTopology: true});
+
+ requireDir('./src/models');
+
+ const Product = mongoose.model('Product');
+
 
 //Primeira Rota
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    Product.create({
+        title: 'React Native',
+        description: 'Build Native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+
+    return res.send('Hello World');
 });
 //toda vez que o usuario acessar a rota.
     //req simboliza a requisição feita ao servidor.
