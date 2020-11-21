2c8e2b878a81d3eb8a0eece5be68d14a5d87d7ba
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-11-20 23:11:15 -0300

    create routes.js + ProductController.js + {/products}

--- Cursos_Rocketseat/Node/server.js
+++ Cursos_Rocketseat/Node/server.js
@@ -9,29 +9,13 @@ const app = express();
 //constante app executando a função express
 
 //Conectando o DB
-mongoose.connect('mongodb://localhost:27017/mongoDB',
+mongoose.connect('mongodb://localhost:27017/mongodb',
  {useNewUrlParser: true,  useUnifiedTopology: true});
 
- requireDir('./src/models');
+requireDir('./src/models');
 
- const Product = mongoose.model('Product');
-
-
-//Primeira Rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build Native apps with React',
-        url: 'http://github.com/facebook/react-native'
-    });
-
-    return res.send('Hello World');
-});
-//toda vez que o usuario acessar a rota.
-    //req simboliza a requisição feita ao servidor.
-    //req vai conter todas as informações possiveis desta requisição.
-        //Parametros, corpo da requisição, cabeçalho da requisição, usuario que fez a requisição, autenticação, IP.
-    //res se refere a resposta dada mediante a requisição.
+//Rotas
+app.use('/',require('./src/routes'));
 
 app.listen(7050);
 //fazendo com que a aplicação ouça a porta 7050
---
+++ Cursos_Rocketseat/Node/src/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const ProductController = require('./controllers/ProductController');
+
+routes.get('/products', ProductController.index);
+
+module.exports = routes;
+
+
+
+
+//toda vez que o usuario acessar a rota.
+    //req simboliza a requisição feita ao servidor.
+    //req vai conter todas as informações possiveis desta requisição.
+        //Parametros, corpo da requisição, cabeçalho da requisição, usuario que fez a requisição, autenticação, IP.
+    //res se refere a resposta dada mediante a requisição.
