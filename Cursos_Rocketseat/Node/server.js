f83038c75f4bab3a79b8ef49c7650725490b7c07
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-11-21 22:34:59 -0300

    criação de registros + crud + paginação + cors

--- Cursos_Rocketseat/Node/server.js
+++ Cursos_Rocketseat/Node/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-//importando o express
+const cors = require('cors');
 const mongoose = require('mongoose');
-//importando o mongoose
 const requireDir = require('require-dir');
 
 //Iniciando o App
 const app = express();
 //constante app executando a função express
+app.use(express.json());
+//permitindo que eu envia dados em json para minha aplicação
+app.use(cors());
 
 //Conectando o DB
 mongoose.connect('mongodb://localhost:27017/mongodb',
--- Cursos_Rocketseat/Node/src/routes.js
+++ Cursos_Rocketseat/Node/src/routes.js
@@ -5,11 +5,13 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
+
 
 module.exports = routes;
-
-
-
 
 //toda vez que o usuario acessar a rota.
     //req simboliza a requisição feita ao servidor.
